fb2a86aaae1c0805d9906becd608599860b2f1f8
dyus <dyuuus@gmail.com>
2024-06-12 23:15:27 +0300

    add storage config

--- app/config.go
+++ app/config.go
@@ -4,6 +4,12 @@ type HTTPConfig struct {
 	Addr string `config:"addr"`
 }
 
+type StorageConfig struct {
+	Num         int    `config:"num_of_storages"`
+	StorageType string `config:"type"`
+}
+
 type Config struct {
-	HTTP HTTPConfig `config:"http"`
+	HTTP    HTTPConfig    `config:"http"`
+	Storage StorageConfig `config:"storage"`
 }
---
+++ internal/storage/storage.go
@@ -0,0 +1,31 @@
+package storage
+
+import (
+	"errors"
+	"fmt"
+	"io"
+)
+
+type Storage interface {
+	UploadFile(string, io.Reader) error
+	ReadFile(string) io.ReadCloser
+}
+
+type StorageService struct {
+	storages map[int]Storage
+}
+
+func NewStorageService(storageType string, num int) (*StorageService, error) {
+	if storageType != "InMemory" {
+		return nil, errors.New(fmt.Sprintf("Unknown type %s", storageType))
+	}
+
+	storages := make(map[int]Storage)
+	for num := range num {
+		storages[num] = NewInMemory()
+	}
+
+	return &StorageService{
+		storages: storages,
+	}, nil
+}
